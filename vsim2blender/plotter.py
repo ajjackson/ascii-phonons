fd8615dce3b5062e12cdadb245e74c31f8266ea7
Adam J. Jackson <a.j.jackson@bath.ac.uk>
2015-07-25 19:49:41 +0100

    notes on displacement formula

--- vsim2blender/plotter.py
+++ vsim2blender/plotter.py
@@ -26,6 +26,29 @@ def draw_bounding_box(cell):
     box_material.use_shadeless = True
 
 
+# Computing the positions
+#
+# Key equation is:
+# _u_(jl,t) = Sum over _k_, nu: _U_(j,_k_,nu) exp(i[_k_ _r_(jl) - w(_k_,nu)t])
+# [M. T. Dove, Introduction to Lattice Dynamics (1993) Eqn 6.18]
+#
+# Where nu is the mode identity, w is frequency, _U_ is the
+# displacement vector, and _u_ is the displacement of atom j in unit
+# cell l. We can break this down to a per-mode displacement and so the
+# up-to-date position of atom j in cell l in a given mode visualisation
+#
+# _r'_(jl,t,nu) = _r_(jl) +  _U_(j,_k_,nu) exp(i[_k_ _r_(jl) - w(_k_,nu) t])
+#
+# Our unit of time should be such that a full cycle elapses over the
+# desired number of frames. 
+#
+# A full cycle usually lasts 2*pi/w, so let t = frame*2*pi/wN;
+# -w t becomes -w 2 pi frame/wN = 2 pi frame / N
+#
+# _r'_(jl,t,nu) = _r_(jl) + _U_(j,_k_,nu) exp(i[_k_ _r_(jl) - 2 pi (frame#)/N])
+#
+#
+
 def main():
     
     ascii_file='gamma_vibs.ascii'
