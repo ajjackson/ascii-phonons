ac6d0dd282ccb1d6e3fa4a7625499a4933b561b5
Adam J. Jackson <a.j.jackson@physics.org>
2016-02-08 12:32:00 +0000

    blender metadata for version number, issue tracker

--- addons/vsim2blender/__init__.py
+++ addons/vsim2blender/__init__.py
@@ -2,8 +2,9 @@ bl_info = {
     "name": "ascii phonons",
     "description": "Generate phonon mode visualisations from ASCII input files",
     "author": "Adam J. Jackson",
-    "version": (0,2),
+    "version": (0,2,1),
     "blender": (2, 65, 0),
     "location": "",
-    "category": "Import-Export"
+    "category": "Import-Export",
+    "tracker_url": "https://github.com/ajjackson/ascii-phonons/issues"
     }
